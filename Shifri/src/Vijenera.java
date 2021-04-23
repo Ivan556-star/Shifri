b3f4f4a6a6becedc1c5e6eb027d74146cc610d65
Ivan556-star <rastoma1249@gmail.com>
2021-04-24 01:05:46 +0300

    Доделал шифр цезаря и делаю шифр Виженера

--- Shifri/src/Vijenera.java
+++ Shifri/src/Vijenera.java
@@ -3,97 +3,69 @@ public class Vijenera implements shifr {
     @Override
     public String[] crypt(int choiceShifr, String text, String KEY) {
         String unswer = "";
-        text = text.toUpperCase();
-        KEY = KEY.toUpperCase();
+        text = text.toUpperCase().replace(" ", "_");
+        KEY = KEY.toUpperCase().replace(" ", "_");
 
         if (choiceShifr == 1){
-            int k = 0;
             while (KEY.length() < text.length()){
-                if (RU().contains(String.valueOf(text.charAt(k)))){
+                if (RU().contains(String.valueOf(text.charAt(KEY.length())))){
                     KEY += RU().charAt(r.nextInt(RU().length()));
                 }
-                if (EN().contains(String.valueOf(text.charAt(k)))){
+                if (EN().contains(String.valueOf(text.charAt(KEY.length())))){
                     KEY += EN().charAt(r.nextInt(EN().length()));
                 }
-                if (SIM().contains(String.valueOf(text.charAt(k)))){
+                if (SIM().contains(String.valueOf(text.charAt(KEY.length())))){
                     KEY += SIM().charAt(r.nextInt(SIM().length()));
                 }
-                k++;
             }
 
-            int indexTMP;
-            for (int i = 0; i < text.length(); i++) {
+            int indexTMP, indexKEY;
+            for (int i = 0; i <text.length(); i++) {
                 if (RU().contains(String.valueOf(text.charAt(i)))){
                     indexTMP = RU().indexOf(String.valueOf(text.charAt(i)));
-                    unswer += RU().
-                            charAt(Math.abs((indexTMP + RU().
-                                    indexOf(String.valueOf(KEY.charAt(i)))) % RU().length()));
+                    indexKEY = RU().indexOf(String.valueOf(KEY.charAt(i)));
+                    unswer += RU().charAt(Math.abs((indexTMP + indexKEY) % RU().length()));
                 }
                 else if (EN().contains(String.valueOf(text.charAt(i)))){
                     indexTMP = EN().indexOf(String.valueOf(text.charAt(i)));
-                    unswer += EN().
-                            charAt(Math.abs((indexTMP + EN().
-                                    indexOf(String.valueOf(KEY.charAt(i)))) % EN().length()));
+                    indexKEY = EN().indexOf(String.valueOf(KEY.charAt(i)));
+                    unswer += EN().charAt(Math.abs((indexTMP + indexKEY) % EN().length()));
                 }
                 else if (SIM().contains(String.valueOf(text.charAt(i)))){
                     indexTMP = SIM().indexOf(String.valueOf(text.charAt(i)));
-                    unswer += SIM().
-                            charAt(Math.abs((indexTMP + SIM().
-                                    indexOf(String.valueOf(KEY.charAt(i)))) % SIM().length()));
+                    indexKEY = SIM().indexOf(String.valueOf(KEY.charAt(i)));
+                    unswer += SIM().charAt(Math.abs((indexTMP + indexKEY) % SIM().length()));
                 }
                 else
-                    System.out.println("Error");
+                    System.out.println("Error VIjener");
             }
-
-            unswer = KEY + unswer;
-            if (99 < KEY.length() && KEY.length() < 1000)
-                KEY = String.valueOf(KEY.length());
-            else if (9 < KEY.length() && KEY.length() < 100)
-                KEY = "0" + KEY.length();
-            else
-                KEY = "00" + KEY.length();
-
-            return new String[]{unswer, KEY};
         }
 
         else {
 
-            String keyShifr = MyCopy(0, Integer.parseInt(KEY), text);
-            text= MyCopy(Integer.parseInt(KEY), text.length(), text);
-            if (keyShifr.length() < text.length()) {
-                text = MyCopy(0, text.length() - 1, text);
-            }
+            if (KEY.length() < text.length())
+                text = MyCopy(0, KEY.length(), text);
+            else if (KEY.length() > text.length())
+                KEY = MyCopy(0, KEY.length(), KEY);
 
-            while (keyShifr.length() > text.length()){
-                int tmp = r.nextInt(3);
-                if (tmp == 0)
-                    text += RU().charAt(r.nextInt(RU().length()));
-                else if (tmp == 1)
-                    text += EN().charAt(r.nextInt(EN().length()));
-                else
-                    text += SIM().charAt(r.nextInt(SIM().length()));
-            }
 
             try {
-                int indexTMP;
+                int indexTMP, indexKEY;
                 for (int i = 0; i <text.length(); i++) {
                     if (RU().contains(String.valueOf(text.charAt(i)))){
                         indexTMP = RU().indexOf(String.valueOf(text.charAt(i)));
-                        unswer += RU().
-                                charAt(Math.abs((indexTMP - RU().
-                                        indexOf(String.valueOf(keyShifr.charAt(i)))) % RU().length()));
+                        indexKEY = RU().indexOf(String.valueOf(KEY.charAt(i)));
+                        unswer += RU().charAt((indexTMP + RU().length() - indexKEY) % RU().length());
                     }
                     else if (EN().contains(String.valueOf(text.charAt(i)))){
                         indexTMP = EN().indexOf(String.valueOf(text.charAt(i)));
-                        unswer += EN().
-                                charAt(Math.abs((indexTMP - EN().
-                                        indexOf(String.valueOf(keyShifr.charAt(i)))) % EN().length()));
+                        indexKEY = EN().indexOf(String.valueOf(KEY.charAt(i)));
+                        unswer += EN().charAt((indexTMP + EN().length() - indexKEY) % EN().length());
                     }
                     else if (SIM().contains(String.valueOf(text.charAt(i)))){
                         indexTMP = SIM().indexOf(String.valueOf(text.charAt(i)));
-                        unswer += SIM().
-                                charAt(Math.abs((indexTMP - SIM().
-                                        indexOf(String.valueOf(keyShifr.charAt(i)))) % SIM().length()));
+                        indexKEY = SIM().indexOf(String.valueOf(KEY.charAt(i)));
+                        unswer += SIM().charAt((indexTMP + SIM().length() - indexKEY) % SIM().length());
                     }
                     else
                         System.out.println("Ошибка, вы ввели символ, которого нет в стандартной клавиатуре");
@@ -101,9 +73,43 @@ public class Vijenera implements shifr {
             }catch (IndexOutOfBoundsException e){
                 return new String[]{"Ошибка, вы указали неверный ключ!", KEY};
             }
-
-            KEY = MyCopy(0, KEY.length() - 2, KEY);
-            return new String[]{unswer, KEY};
         }
+        return new String[]{unswer, KEY};
     }
+
+    public static void main(String[] args) {
+        Vijenera cypher = new Vijenera();
+        int choiceShifr;
+        while (true)
+            try {
+                System.out.print("Зашифровать(1)/расшифровать(0): ");
+                String choiceShifrIn = cin.nextLine();
+                choiceShifr = Integer.parseInt(choiceShifrIn);
+                if (choiceShifr < 0 || choiceShifr > 1) {
+                    System.out.println("Ошибка, введите 1 или 0");
+                    continue;
+                }
+                break;
+            } catch (Exception e) {
+                System.out.println("Ошибка, введите 1 или 0");
+            }
+
+        String KEY;
+        System.out.print("Введите ключ: ");
+        KEY = cin.nextLine();
+        System.out.println("Введите текст");
+        String text = cin.nextLine();
+        String[] unswer = cypher.crypt(choiceShifr, text, KEY);
+        if (choiceShifr == 1)
+            System.out.println("Зашифрованный текст\n" +  unswer[0] +
+                    "\nКлюч от шифра\n" + unswer[1]);
+        else
+            System.out.println("Расшированный текст\n" +  unswer[0] +
+                    "\nКлюч от шифра\n" + unswer[1]);
+
+        // FRQ
+        // HELLO_WORLD!
+    }
+
+
 }
